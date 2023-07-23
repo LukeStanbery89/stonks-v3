2a970c08083fe3e629446323ba5abef049e56d06
Luke Stanbery <lukestanbery89@gmail.com>
2023-07-23 16:18:34 -0500

    fix server-side errors caused by dynamic imports

--- src/subscribers/sockets/index.ts
+++ src/subscribers/sockets/index.ts
@@ -1,17 +1,11 @@
-import fs from "fs";
-import path from "path";
+import my_websocket_event from "./handlers/my_websocket_event";
+import my_other_websocket_event from "./handlers/my_other_websocket_event";
+// Import other handlers as needed
 
-const handlers: { [key: string]: any } = {};
-
-const handlersDir = path.join(__dirname, "handlers");
-const files = fs.readdirSync(handlersDir);
-
-files.forEach((file) => {
-    const handlerName = path.parse(file).name;
-    const HandlerPath = path.join(handlersDir, file);
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const handler = require(HandlerPath).default;
-    handlers[handlerName] = handler;
-});
+const handlers: { [key: string]: any } = {
+    my_websocket_event,
+    my_other_websocket_event,
+    // Add other handlers here
+};
 
 export default handlers;
