33f38bedec174141e880bb732e08b7a6454f832b
Luke Stanbery <lukestanbery89@gmail.com>
2024-01-16 00:56:54 -0600

    improve src/lib/utils.ts to 100% coverage

---
+++ src/tests/lib/utils.test.ts
@@ -0,0 +1,17 @@
+import { sleep } from "../../lib/utils";
+
+describe("Utils", () => {
+    describe("sleep", () => {
+        it("should pause the program for the specified time", async () => {
+            const startTime = Date.now();
+            const sleepTime = 1000; // 1 second
+
+            await sleep(sleepTime);
+
+            const endTime = Date.now();
+            const elapsedTime = endTime - startTime;
+
+            expect(elapsedTime).toBeGreaterThanOrEqual(sleepTime);
+        });
+    });
+});
