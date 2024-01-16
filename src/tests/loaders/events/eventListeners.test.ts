0015348fe3f1a6f09f880f830eaa056132bcf2d7
Luke Stanbery <lukestanbery89@gmail.com>
2024-01-16 02:11:22 -0600

    write tests for eventListeners.ts

---
+++ src/tests/loaders/events/eventListeners.test.ts
@@ -0,0 +1,27 @@
+import { registerEventListeners } from "../../../loaders/events/eventListeners";
+import eventEmitter from "../../../lib/eventEmitter";
+
+jest.mock(
+    "../../../subscribers/events/handlers/index.ts",
+    jest.fn(() => {
+        return {
+            mock_handler: jest.fn(),
+        };
+    }),
+    { virtual: true },
+);
+
+describe("eventListeners", () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it("should register event listeners correctly", () => {
+        const mockEventEmitter = jest.spyOn(eventEmitter, "on");
+
+        registerEventListeners();
+
+        expect(mockEventEmitter).toHaveBeenCalledTimes(1);
+        expect(mockEventEmitter).toHaveBeenCalledWith("mock_handler", expect.any(Function));
+    });
+});
