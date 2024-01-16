541b9911edada2846285ba5e02f8f77a0aac7e19
Luke Stanbery <lukestanbery89@gmail.com>
2024-01-15 20:04:07 -0600

    write tests for BrokerageProvider

---
+++ src/tests/lib/brokerage/BrokerageProvider.test.ts
@@ -0,0 +1,176 @@
+import ConcreteBrokerageProvider from "./ConcreteBrokerageProvider";
+
+describe("Brokerage Provider", () => {
+    beforeAll(() => {
+        console.log = jest.fn();
+    });
+
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
+
+    describe("convertToSecuritiesArray()", () => {
+        it("should convert the provider's security type to the app's security type", () => {
+            const brokerageProvider = new ConcreteBrokerageProvider();
+            const providerSecurity = {
+                symbol: "AAPL",
+                name: "Apple",
+            };
+            const expectedSecurity = {
+                symbol: "AAPL",
+                name: "Apple",
+                transformed: true,
+            };
+            const securities = brokerageProvider.testConvertToSecuritiesArray([providerSecurity]);
+            expect(securities).toEqual([expectedSecurity]);
+        });
+    });
+
+    describe("convertToPriceDataArray()", () => {
+        it("should convert the provider's price data type to the app's price data type", () => {
+            const brokerageProvider = new ConcreteBrokerageProvider();
+            const providerPriceData = [
+                { timestamp: "2022-01-01", open: 100, high: 110, low: 90, close: 105, volume: 0 },
+                { timestamp: "2022-01-02", open: 105, high: 115, low: 95, close: 110, volume: 0 },
+            ];
+            const expectedPriceData = [
+                { timestamp: "2022-01-01", open: 100, high: 110, low: 90, close: 105, volume: 0, transformed: true },
+                { timestamp: "2022-01-02", open: 105, high: 115, low: 95, close: 110, volume: 0, transformed: true },
+            ];
+            const priceData = brokerageProvider.testConvertToPriceDataArray(providerPriceData);
+            expect(priceData).toEqual(expectedPriceData);
+        });
+    });
+
+    describe("getSecuritiesUri()", () => {
+        it("should return the URI for the securities endpoint", () => {
+            const brokerageProvider = new ConcreteBrokerageProvider();
+            const expectedURI = "https://dev.example.com/securities";
+            const uri = brokerageProvider.testGetSecuritiesUri();
+            expect(uri).toEqual(expectedURI);
+        });
+    });
+
+    describe("getBuyUri()", () => {
+        it("should return the URI for the buy endpoint", () => {
+            const brokerageProvider = new ConcreteBrokerageProvider();
+            const expectedURI = "https://dev.example.com/buy";
+            const uri = brokerageProvider.testGetBuyUri();
+            expect(uri).toEqual(expectedURI);
+        });
+    });
+
+    describe("getSellUri()", () => {
+        it("should return the URI for the sell endpoint", () => {
+            const brokerageProvider = new ConcreteBrokerageProvider();
+            const expectedURI = "https://dev.example.com/sell";
+            const uri = brokerageProvider.testGetSellUri();
+            expect(uri).toEqual(expectedURI);
+        });
+    });
+
+    describe("getHistoricalPriceDataUri", () => {
+        it("should return the URI for the historical price data endpoint", () => {
+            const brokerageProvider = new ConcreteBrokerageProvider();
+            const expectedURI = "https://dev.example.com/historical-price-data";
+            const uri = brokerageProvider.testGetHistoricalPriceDataUri();
+            expect(uri).toEqual(expectedURI);
+        });
+    });
+
+    describe("getSecuritiesQueryStringParams()", () => {
+        it("should return the query string params for the securities endpoint", () => {
+            const brokerageProvider = new ConcreteBrokerageProvider();
+            const expectedQueryStringParams = "foo=bar";
+            const queryStringParams = brokerageProvider.testGetSecuritiesQueryStringParams();
+            expect(queryStringParams).toEqual(expectedQueryStringParams);
+        });
+    });
+
+    describe("getBuyQueryStringParams()", () => {
+        it("should return the query string params for the buy endpoint", () => {
+            const brokerageProvider = new ConcreteBrokerageProvider();
+            const expectedQueryStringParams = "foo=bar";
+            const queryStringParams = brokerageProvider.testGetBuyQueryStringParams({} as any);
+            expect(queryStringParams).toEqual(expectedQueryStringParams);
+        });
+    });
+
+    describe("getSellQueryStringParams()", () => {
+        it("should return the query string params for the sell endpoint", () => {
+            const brokerageProvider = new ConcreteBrokerageProvider();
+            const expectedQueryStringParams = "foo=bar";
+            const queryStringParams = brokerageProvider.testGetSellQueryStringParams({} as any);
+            expect(queryStringParams).toEqual(expectedQueryStringParams);
+        });
+    });
+
+    describe("getHistoricalPriceDataQueryStringParams()", () => {
+        it("should return the query string params for the historical price data endpoint", () => {
+            const brokerageProvider = new ConcreteBrokerageProvider();
+            const expectedQueryStringParams = "foo=bar";
+            const queryStringParams = brokerageProvider.testGetHistoricalPriceDataQueryStringParams({} as any);
+            expect(queryStringParams).toEqual(expectedQueryStringParams);
+        });
+    });
+
+    describe("getRequestHeaders()", () => {
+        it("should return the request headers", () => {
+            const brokerageProvider = new ConcreteBrokerageProvider();
+            const expectedHeaders = {
+                foo: "bar",
+                accept: "application/json",
+            };
+            const headers = brokerageProvider.testGetRequestHeaders();
+            expect(headers).toEqual(expectedHeaders);
+        });
+    });
+
+    describe("getSecuritiesHeaders()", () => {
+        it("should return the headers for the securities endpoint", () => {
+            const brokerageProvider = new ConcreteBrokerageProvider();
+            const expectedHeaders = {
+                foo: "bar",
+                accept: "application/json",
+            };
+            const headers = brokerageProvider.testGetSecuritiesHeaders();
+            expect(headers).toEqual(expectedHeaders);
+        });
+    });
+
+    describe("getBuyHeaders()", () => {
+        it("should return the headers for the buy endpoint", () => {
+            const brokerageProvider = new ConcreteBrokerageProvider();
+            const expectedHeaders = {
+                foo: "bar",
+                accept: "application/json",
+            };
+            const headers = brokerageProvider.testGetBuyHeaders();
+            expect(headers).toEqual(expectedHeaders);
+        });
+    });
+
+    describe("getSellHeaders()", () => {
+        it("should return the headers for the sell endpoint", () => {
+            const brokerageProvider = new ConcreteBrokerageProvider();
+            const expectedHeaders = {
+                foo: "bar",
+                accept: "application/json",
+            };
+            const headers = brokerageProvider.testGetSellHeaders();
+            expect(headers).toEqual(expectedHeaders);
+        });
+    });
+
+    describe("getHistoricalPriceDataHeaders()", () => {
+        it("should return the headers for the historical price data endpoint", () => {
+            const brokerageProvider = new ConcreteBrokerageProvider();
+            const expectedHeaders = {
+                foo: "bar",
+                accept: "application/json",
+            };
+            const headers = brokerageProvider.testGetHistoricalPriceDataHeaders();
+            expect(headers).toEqual(expectedHeaders);
+        });
+    });
+});
---
+++ src/tests/lib/brokerage/ConcreteBrokerageProvider.ts
@@ -0,0 +1,124 @@
+import BrokerageProvider from "../../../lib/brokerage/BrokerageProvider";
+import { BuyOrder, BuyResult, HistoricalPriceDataRequestParams, PriceData, Security, SellOrder, SellResult } from "../../../types/types";
+
+type Transformable = {
+    transformed: boolean,
+};
+type TestSecurity = Security & Transformable;
+type TestBuyResult = BuyResult & Transformable;
+type TestSellResult = SellResult & Transformable;
+type TestPriceData = PriceData & Transformable;
+
+class ConcreteBrokerageProvider extends BrokerageProvider {
+    // Test implementations of abstract props
+    protected defaultHeaders = {
+        foo: "bar",
+        accept: "application/json",
+    };
+    protected providerCommonHeaders = {};
+    protected devAPIDomain = "https://dev.example.com";
+    protected prodAPIDomain = "https://prod.example.com";
+    protected securitiesPath = "/securities";
+    protected buyPath = "/buy";
+    protected sellPath = "/sell";
+    protected historicalPriceDataURIPath = "/historical-price-data";
+    protected securitiesQueryStringParams = "foo=bar";
+    protected buyQueryStringParams = "foo=bar";
+    protected sellQueryStringParams = "foo=bar";
+    protected historicalPriceDataQueryStringParams = "foo=bar";
+
+    // Test implementations of abstract methodsç
+    protected convertBuyOrderToRequestData(buyOrder: BuyOrder): object {
+        return {
+            ...buyOrder,
+            transformed: true,
+        };
+    }
+    protected convertSellOrderToRequestData(sellOrder: SellOrder): object {
+        return {
+            ...sellOrder,
+            transformed: true,
+        };
+    }
+    protected convertToSecurity(security: Security): TestSecurity {
+        return {
+            ...security,
+            transformed: true,
+        };
+    }
+    protected convertToBuyResult(buyResult: BuyResult): TestBuyResult {
+        return {
+            ...buyResult,
+            transformed: true,
+        };
+    }
+    protected convertToSellResult(sellResult: SellResult): TestSellResult {
+        return {
+            ...sellResult,
+            transformed: true,
+        };
+    }
+    protected convertToPriceData(priceData: PriceData): TestPriceData {
+        return {
+            ...priceData,
+            transformed: true,
+        };
+    }
+    public historicalPriceData(priceDataParams: HistoricalPriceDataRequestParams): Promise<PriceData[]> {
+        return Promise.resolve(
+            [
+                { timestamp: "2022-01-01", open: 100, high: 110, low: 90, close: 105, volume: 0 },
+                { timestamp: "2022-01-02", open: 105, high: 115, low: 95, close: 110, volume: 0 },
+            ],
+        );
+    }
+
+    // Test helper methods
+    public testConvertToSecuritiesArray(securities: Security[]): Security[] {
+        return this.convertToSecuritiesArray(securities);
+    }
+    public testConvertToPriceDataArray(priceData: PriceData[]): PriceData[] {
+        return this.convertToPriceDataArray(priceData);
+    }
+    public testGetSecuritiesUri(): string {
+        return this.getSecuritiesUri();
+    }
+    public testGetBuyUri(): string {
+        return this.getBuyUri();
+    }
+    public testGetSellUri(): string {
+        return this.getSellUri();
+    }
+    public testGetHistoricalPriceDataUri(): string {
+        return this.getHistoricalPriceDataUri();
+    }
+    public testGetSecuritiesQueryStringParams(): string {
+        return this.getSecuritiesQueryStringParams();
+    }
+    public testGetBuyQueryStringParams(buyOrder: BuyOrder): string {
+        return this.getBuyQueryStringParams(buyOrder);
+    }
+    public testGetSellQueryStringParams(sellOrder: SellOrder): string {
+        return this.getSellQueryStringParams(sellOrder);
+    }
+    public testGetHistoricalPriceDataQueryStringParams(priceDataParams: HistoricalPriceDataRequestParams): string {
+        return this.getHistoricalPriceDataQueryStringParams(priceDataParams);
+    }
+    public testGetRequestHeaders(): object {
+        return this.getRequestHeaders();
+    }
+    public testGetSecuritiesHeaders(): object {
+        return this.getSecuritiesHeaders();
+    }
+    public testGetBuyHeaders(): object {
+        return this.getBuyHeaders();
+    }
+    public testGetSellHeaders(): object {
+        return this.getSellHeaders();
+    }
+    public testGetHistoricalPriceDataHeaders(): object {
+        return this.getHistoricalPriceDataHeaders();
+    }
+}
+
+export default ConcreteBrokerageProvider;
