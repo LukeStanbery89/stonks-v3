246a6a88330cc670be99a7befa91add22867bfac
Luke Stanbery <lukestanbery89@gmail.com>
2024-05-05 14:14:02 -0500

    write new Logger class

---
+++ src/lib/Logger.ts
@@ -0,0 +1,23 @@
+/**
+ * Logger class
+ *
+ * For now, this is just a wrapper around console.log, but it
+ * could be expanded to include more functionality in the future.
+ */
+export default class Logger {
+    public static log(...message: any[]): void {
+        console.log(...message);
+    }
+
+    public static error(...message: any[]): void {
+        console.error(...message);
+    }
+
+    public static warn(...message: any[]): void {
+        console.warn(...message);
+    }
+
+    public static info(...message: any[]): void {
+        console.info(...message);
+    }
+}
