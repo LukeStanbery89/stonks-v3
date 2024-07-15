f7c9cd38b9741c5748a8594ae46c389e7e2ad92e
Luke Stanbery <lukestanbery89@gmail.com>
2024-07-15 18:38:00 -0500

    mock the sleep() function during tests

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,5 +1,11 @@
 /* eslint-disable no-console */
+// Avoid printing logs during tests
 console.log = jest.fn();
 console.error = jest.fn();
 console.warn = jest.fn();
 console.info = jest.fn();
+
+// We want to avoid pausing the tests, so we need to mock the sleep function
+jest.mock("../lib/utils", () => ({
+    sleep: jest.fn(),
+}));
